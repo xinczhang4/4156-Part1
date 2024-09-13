2244e485c1c63f59eabc48ce9c435de83d883b0d
xinczhang4 <xinczhang4-c@my.cityu.edu.hk>
2024-09-13 04:29:22 -0400

    Course.java bug cleared

--- IndividualProject/src/main/java/dev/coms4156/project/individualproject/Course.java
+++ IndividualProject/src/main/java/dev/coms4156/project/individualproject/Course.java
@@ -23,7 +23,7 @@ public class Course implements Serializable {
     this.instructorName = instructorName;
     this.courseTimeSlot = timeSlot;
     this.enrollmentCapacity = capacity;
-    this.enrolledStudentCount = 500;
+    this.enrolledStudentCount = 0;
   }
 
   /**
@@ -32,8 +32,10 @@ public class Course implements Serializable {
    * @return true if the student is successfully enrolled, false otherwise.
    */
   public boolean enrollStudent() {
+    if(isCourseFull())
+      return false;
     enrolledStudentCount++;
-    return false;
+    return true;
   }
 
   /**
@@ -42,18 +44,21 @@ public class Course implements Serializable {
    * @return true if the student is successfully dropped, false otherwise.
    */
   public boolean dropStudent() {
-    enrolledStudentCount--;
+    if(enrolledStudentCount > 0){
+      enrolledStudentCount--;
+      return true;
+    }
     return false;
   }
 
 
   public String getCourseLocation() {
-    return this.instructorName;
+    return this.courseLocation;
   }
 
 
   public String getInstructorName() {
-    return this.courseLocation;
+    return this.instructorName;
   }
 
 
@@ -94,7 +99,7 @@ public class Course implements Serializable {
 
 
   public boolean isCourseFull() {
-    return enrollmentCapacity > enrolledStudentCount;
+    return enrolledStudentCount >= enrollmentCapacity;
   }
 
   @Override
